d3078f0f81ba5a2acd2f709965dc37a16d002f5b
Valentin Baric <Valentin.Baric@posteo.de>
2019-11-02 22:21:14 +0100

    add folder for interfaces

--- DirectDepositionAlgorithm/util/CalcDistance.cs
+++ DirectDepositionAlgorithm/util/CalcDistance.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using DirectDepositionAlgorithm;
+using System;
+
 namespace AggregatedNanoparticleFilmFormation.util
 {
-    public class CalcDistance
+    public static class CalcDistance
     {
-        public CalcDistance()
+        public static double DistanceSquared(PrimaryParticle particle1,PrimaryParticle particle2)
         {
+            var x = Math.Pow(particle2.X - particle1.X, 2);
+            var y = Math.Pow(particle2.Y - particle1.Y, 2);
+            var z = Math.Pow(particle2.Z - particle1.Z, 2);
+            return x + y + z;
+        }
+
+        public static double Distance(PrimaryParticle particle1, PrimaryParticle particle2)
+        {
+            var x = Math.Pow(particle2.X - particle1.X, 2);
+            var y = Math.Pow(particle2.Y - particle1.Y, 2);
+            var z = Math.Pow(particle2.Z - particle1.Z, 2);
+            return Math.Sqrt(x + y + z);
         }
     }
 }
